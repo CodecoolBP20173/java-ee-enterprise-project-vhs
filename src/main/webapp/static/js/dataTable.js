069fc9914926cdfb588ee28554d99bc336004781
Adel Kovacs <kovacsadel12@gmail.com>
2018-05-30 15:39:17 +0200

    js for jumbotron

--- src/main/webapp/static/js/dataTable.js
+++ src/main/webapp/static/js/dataTable.js
@@ -2,6 +2,21 @@
 $(document).ready( function () {
     let firstCarouselItem = $('.carousel-item:first');
     firstCarouselItem.addClass('active');
-
     $('#movies_list').DataTable();
+
+    let searchBar = $('label:last');
+    let toReplace = $('#searchReplace');
+    toReplace.replaceWith(searchBar);
+
+    let searchInput = $('input:first');
+    console.log(searchInput);
+    // searchInput.onfocus = function (e) {
+    //     let jumbotron = $('.jumbotron');
+    //     jumbotron.hide();
+    // };
+
+    document.getElementsByTagName("input")[0].addEventListener("click", function() {
+        document.getElementById("jumbo").remove();
+    });
+
 } );
