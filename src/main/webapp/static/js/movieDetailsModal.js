d21d773fcfd8de26ebba3afc1f122ff05e487a92
peterBarazutti <barazutti.peter@gmail.com>
2018-05-31 18:17:17 +0200

    Added custom "design" to modal window, Refactored: removed dead code

--- src/main/webapp/static/js/movieDetailsModal.js
+++ src/main/webapp/static/js/movieDetailsModal.js
@@ -1,16 +1,11 @@
-//$(".btn-movie-details").click(function (clicked_button) {
 function populateModal(id) {
-        //var movieId = clicked_button.target.getAttribute('id').substring(5);
         $.ajax({
             type: 'POST',
             url: '/movie-details',
             data: {'movieId': id},
             success: function (data) {
-                //showMovieDetails(data),
                 console.log(data);
                 showMovieDetails(data)
-                /*$('#movie_details_modal').append('<div>' + data.title +'</div>');
-                $('#movie_details_modal').append('<div>' + data.releaseDate +'</div>');*/
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log("jqXHR : " + jqXHR + " text status : " + textStatus + " error : " + errorThrown);
@@ -27,7 +22,7 @@ function showMovieDetails(data) {
         '<div><h4>' + data.title + '</h4></div>' +
         '<div>' + data.releaseDate + '</div>' +
         '<div>' + data.description + '</div>' +
-        '<img' + ' src="static/img/movie' + data.id + '.jpg"' + 'width="60px" >');
+        '<img' + ' src="static/img/movie' + data.id + '.jpg"' + 'width="120px" >');
     $("#exampleModal").modal("show");
 }
 
