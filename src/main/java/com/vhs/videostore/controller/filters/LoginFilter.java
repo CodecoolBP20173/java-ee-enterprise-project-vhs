f32b16bd84196344582e68cc27b1b9798a6cd88f
peterBarazutti <barazutti.peter@gmail.com>
2018-06-12 12:53:42 +0200

    User page can only be opened by its owner

--- src/main/java/com/vhs/videostore/controller/filters/LoginFilter.java
+++ src/main/java/com/vhs/videostore/controller/filters/LoginFilter.java
@@ -2,7 +2,6 @@ package com.vhs.videostore.controller.filters;
 
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -10,18 +9,21 @@ import java.io.IOException;
 
 public class LoginFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpSession session = ((HttpServletRequest) request).getSession();
-        if (session.getAttribute("userId") == null) {
-            ((HttpServletResponse) response).sendRedirect("/");
-        } else {
+        if (Integer.parseInt(session.getAttribute("userId").toString()) ==
+                (Integer.parseInt(((HttpServletRequest) request).getPathInfo().split("/")[1]))) {
             chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse) response).sendRedirect("/");
         }
     }
 
     @Override
-    public void destroy() {}
+    public void destroy() {
+    }
 }
