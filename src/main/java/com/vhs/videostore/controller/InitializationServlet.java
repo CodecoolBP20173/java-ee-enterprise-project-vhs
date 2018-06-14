0aef8aebdc86851d4c0892dc6999989589cabda9
peterBarazutti <barazutti.peter@gmail.com>
2018-06-14 10:31:29 +0200

    Initializing rental control and service

--- src/main/java/com/vhs/videostore/controller/InitializationServlet.java
+++ src/main/java/com/vhs/videostore/controller/InitializationServlet.java
@@ -1,9 +1,6 @@
 package com.vhs.videostore.controller;
 
-import com.vhs.videostore.services.EMProvider;
-import com.vhs.videostore.services.MainPageService;
-import com.vhs.videostore.services.MovieDetailService;
-import com.vhs.videostore.services.UserPageService;
+import com.vhs.videostore.services.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -19,17 +16,21 @@ public class InitializationServlet extends HttpServlet {
         MainPageService mainPageService = new MainPageService(em);
         MovieDetailService movieDetailService = new MovieDetailService(em);
         UserPageService userPageService = new UserPageService(em);
+        RentalService rentalService = new RentalService(em);
+
         MainPage mainPage = new MainPage(mainPageService);
         MovieDetail movieDetail = new MovieDetail(movieDetailService);
         UserPage userPage = new UserPage(userPageService);
         Login login = new Login(userPageService);
         Registration registration = new Registration(userPageService);
+        Renting renting = new Renting(rentalService);
 
         getServletContext().setAttribute("mainPage", mainPage);
         getServletContext().setAttribute("movieDetail", movieDetail);
         getServletContext().setAttribute("userPage", userPage);
         getServletContext().setAttribute("login", login);
         getServletContext().setAttribute("registration", registration);
+        getServletContext().setAttribute("renting", renting);
 
 
 
