579dd21df24bda90c0ecdf2c7a2049930a2c5384
peterBarazutti <barazutti.peter@gmail.com>
2018-06-26 14:05:44 +0200

    Fixed movie-detail endpoint

--- src/main/java/com/vhs/videostore/api/MoviePageServiceREST.java
+++ src/main/java/com/vhs/videostore/api/MoviePageServiceREST.java
@@ -1,11 +1,17 @@
 package com.vhs.videostore.api;
 
 import com.vhs.videostore.model.Movie;
+import com.vhs.videostore.repository.MovieRepository;
 import com.vhs.videostore.services.MovieDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class MoviePageServiceREST {
@@ -16,9 +22,8 @@ public class MoviePageServiceREST {
         this.movieDetailService = movieDetailService;
     }
 
-    @RequestMapping(value = "/movie", method = RequestMethod.POST)
-    public Movie getMovie(@RequestParam("id") String id) {
-        Long parsedId = Long.parseLong(id);
-        return movieDetailService.findById(parsedId);
+    @RequestMapping(value = "/movie-detail", method = RequestMethod.POST)
+    public ResponseEntity<Movie> getMovie(@RequestParam("movieId") String id) {
+        return new ResponseEntity<>(movieDetailService.findById(Integer.parseInt(id)).get(), HttpStatus.OK);
     }
 }
