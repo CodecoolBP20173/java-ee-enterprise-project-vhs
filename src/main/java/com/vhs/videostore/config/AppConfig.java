698b3058b7ab8cc8511aeb252b7362ea6f0ba405
Unknown <lisztes.alex@gmail.com>
2018-06-27 23:29:06 +0200

    New configuration for app with property sources

--- src/main/java/com/vhs/videostore/config/AppConfig.java
+++ src/main/java/com/vhs/videostore/config/AppConfig.java
@@ -1,42 +1,70 @@
 package com.vhs.videostore.config;
 
-import com.vhs.videostore.controller.Auth0Filter;
+import com.auth0.AuthenticationController;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Component
+import java.io.UnsupportedEncodingException;
+
+@SuppressWarnings("unused")
 @Configuration
-public class AppConfig {
-//    @Value(value = "${com.auth0.domain}")
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * This is your auth0 domain (tenant you have created when registering with auth0 - account name)
+     */
+    @Value(value = "${com.auth0.domain}")
     private String domain;
 
-//    @Value(value = "${com.auth0.clientId}")
+    /**
+     * This is the client id of your auth0 application (see Settings page on auth0 dashboard)
+     */
+    @Value(value = "${com.auth0.clientId}")
     private String clientId;
 
-//    @Value(value = "${com.auth0.clientSecret}")
+    /**
+     * This is the client secret of your auth0 application (see Settings page on auth0 dashboard)
+     */
+    @Value(value = "${com.auth0.clientSecret}")
     private String clientSecret;
 
     @Bean
-    public FilterRegistrationBean filterRegistration() {
-        final FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new Auth0Filter());
-        registration.addUrlPatterns("/portal/*");
-        registration.setName(Auth0Filter.class.getSimpleName());
-        return registration;
+    public AuthenticationController authenticationController() throws UnsupportedEncodingException {
+        return AuthenticationController.newBuilder(domain, clientId, clientSecret)
+                .build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/callback", "/login").permitAll()
+                .antMatchers("/rent-movie").authenticated()
+                .antMatchers("/user/**").authenticated()
+                .and()
+                .logout().permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
     }
 
     public String getDomain() {
-        return "adelkov.eu.auth0.com";
+        return domain;
     }
 
     public String getClientId() {
-        return "TplDSmE36t17MYuGirzDhbPDYpcg1vHy";
+        return clientId;
     }
 
     public String getClientSecret() {
-        return "MffHtKpVERUX4W5L0bvZokpIgylp3OMw_Y3MHkw1puFyLJjiF3NRPHtjeJ7bcbo8";
+        return clientSecret;
     }
 }
