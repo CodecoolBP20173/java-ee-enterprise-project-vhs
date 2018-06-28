ea55848745feff4eecbc26d815ba87039a585c3a
Adel Kovacs <kovacsadel12@gmail.com>
2018-06-28 13:08:36 +0200

    dead code clear-up, navbar button visibility fix

--- src/main/java/com/vhs/videostore/controller/UserPage.java
+++ src/main/java/com/vhs/videostore/controller/UserPage.java
@@ -1,7 +1,5 @@
 package com.vhs.videostore.controller;
 
-//import com.vhs.videostore.services.UserPageService;
-
 import com.vhs.videostore.model.Rental;
 import com.vhs.videostore.services.RentalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +22,6 @@ public class UserPage {
 
     @GetMapping(value = "/user/{id}")
     public String userPageUI(Model model, @PathVariable("id") String _id) {
-
-        //context = Utility.loginFromSession(context, req);
-
-//        User currentUser = userPageService.getUserByID(Integer.parseInt(_id));
         List<Rental> rentalList = rentalService.findRentalsByUserId(_id);
         model.addAttribute("rentals", rentalList);
         model.addAttribute("userId", _id);
